fd90ce5b4096265086f606388e8426a8a83d5ed0
Muhammad Hashir <pablotraveler786@gmail.com>
2026-08-05 08:06:21 -1200

    fix cors for production frontend

--- bells-backend/server.js
+++ bells-backend/server.js
@@ -13,7 +13,7 @@ import productRoutes from "./routes/productRoutes.js";
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://bells-beta.vercel.app"],
+  origin: "https://bells-beta.vercel.app",
   credentials: true,
 }));
 
